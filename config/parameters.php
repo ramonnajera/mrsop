97e265a58a4fdfb37a5f50b545cc1482df749ec8
ramonnajera <ramonnajera@hotmail.com>
2021-04-08 10:52:49 -0600

    se realizan actualizaciones

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,12 @@
 <?php
 
-define('base_url','http://mrsop.com.devel');
+// produccion
+define('base_url','http://mrsop.com');
 define('controller_default', 'pageController');
 define('action_default', 'index');
+
+
+// local
+// define('base_url','http://localhost/mrsop');
+// define('controller_default', 'pageController');
+// define('action_default', 'index');
