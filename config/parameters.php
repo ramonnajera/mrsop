81e793a49a3713f036671a266b74ec74dbeae9d0
ramonnajera <ramonnajera@hotmail.com>
2021-04-08 16:46:11 -0600

    Cambio de parametros a servidor

--- config/parameters.php
+++ config/parameters.php
@@ -1,12 +1,12 @@
 <?php
 
 // produccion
-// define('base_url','http://mrsop.com');
-// define('controller_default', 'pageController');
-// define('action_default', 'index');
+define('base_url','http://mrsop.com');
+define('controller_default', 'pageController');
+define('action_default', 'index');
 
 
 // local
-define('base_url','http://localhost/mrsop');
-define('controller_default', 'pageController');
-define('action_default', 'index');
+// define('base_url','http://localhost/mrsop');
+// define('controller_default', 'pageController');
+// define('action_default', 'index');
